2ff760f24b41bcac99813a43e97a7c6f8088c953
Rastraponovich <wilde@bk.ru>
2023-09-12 11:14:49 +0300

    chore: added absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -17,4 +17,15 @@ export default defineConfig(async () => ({
   // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
+  resolve: {
+    alias: {
+      "~/app": "/src/app",
+      "~/pages": "/src/pages",
+      "~/layouts": "/src/layouts",
+      "~/widgets": "/src/widgets",
+      "~/features": "/src/features",
+      "~/entities": "/src/entities",
+      "~/shared": "/src/shared",
+    },
+  },
 }));

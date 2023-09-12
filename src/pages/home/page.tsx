49083484fa1b528eb5b24c1dd292673be4b5ea95
Rastraponovich <wilde@bk.ru>
2023-09-12 12:17:46 +0300

    chore: added link in main page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,3 +1,7 @@
+import { Link } from "atomic-router-react";
+
+import { routes } from "~/shared/routing";
+
 import Logo from "../../assets/react.svg";
 
 export const HomePage = () => {
@@ -5,6 +9,9 @@ export const HomePage = () => {
     <section className="flex grow flex-col gap-10  text-center">
       <header>
         <h1 className="text-bold text-3xl">Welcome</h1>
+        <Link to={routes.posts.posts}>
+          <a className="text-blue-300 hover:underline">click to start</a>
+        </Link>
       </header>
 
       <img src={Logo} alt="Logo" width={400} height={400} className="animate-pulse self-center" />

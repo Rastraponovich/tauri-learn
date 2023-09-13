de632f9426fe99cbe7712b27fd20f90daad19059
Rastraponovich <wilde@bk.ru>
2023-09-13 10:19:37 +0300

    chore:set grid to #root

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,5 @@
 import { RoutesView } from "../pages";
 
 export const Application = () => {
-  return (
-    <main className="container mx-auto flex h-screen flex-col gap-10 p-10 sm:p-6">
-      <RoutesView />
-    </main>
-  );
+  return <RoutesView />;
 };

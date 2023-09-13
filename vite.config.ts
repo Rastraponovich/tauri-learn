02b3842140f3728d592b3f1ee2d0ffafcd9167fa
Rastraponovich <wilde@bk.ru>
2023-09-13 20:43:09 +0300

    chore: babel config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        babelrc: true,
+      },
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //

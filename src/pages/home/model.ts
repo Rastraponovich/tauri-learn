d46178ba1bec81cb0649fd3f2fd9684c275a78cb
Rastraponovich <wilde@bk.ru>
2023-09-24 15:39:11 +0300

    feat: start modeling feature

--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -1,3 +1,55 @@
+import { combine, createEvent, createStore, sample } from "effector";
+import { reset } from "patronum";
+
 import { routes } from "~/shared/routing";
 
 export const currentRoute = routes.home;
+
+type Transaction = {
+  id: number;
+  amount: number;
+  type?: string;
+  category?: string;
+  date?: string;
+};
+
+export const transactionSubmitted = createEvent();
+export const amountChanged = createEvent<number>();
+export const categoryChanged = createEvent<string>();
+export const dateChanged = createEvent<string>();
+
+export const $transactions = createStore<Transaction[]>([]);
+export const $amount = createStore(0);
+export const $category = createStore("");
+export const $date = createStore(new Date().toLocaleDateString("ru"));
+
+export const $balance = $transactions.map((transactions) => {
+  return transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+});
+
+const $transaction = combine({
+  amount: $amount,
+  category: $category,
+  type: "",
+  date: $date,
+});
+
+$amount.on(amountChanged, (_, amount) => amount);
+$date.on(dateChanged, (_, date) => date);
+
+$category.on(categoryChanged, (_, category) => category);
+
+sample({
+  clock: transactionSubmitted,
+  source: { transaction: $transaction, transactions: $transactions },
+
+  fn: ({ transaction, transactions }) => {
+    return [...transactions, { ...transaction, id: transactions.length }];
+  },
+  target: $transactions,
+});
+
+reset({
+  clock: $transactions,
+  target: [$amount, $category],
+});

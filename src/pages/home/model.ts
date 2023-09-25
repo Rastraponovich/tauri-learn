3f2af5d17186c2b2c24058104c10f90d29405987
Rastraponovich <wilde@bk.ru>
2023-09-25 10:50:28 +0300

    chore: added chartjs and render

--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -5,26 +5,31 @@ import { routes } from "~/shared/routing";
 
 export const currentRoute = routes.home;
 
-type Transaction = {
+export type Transaction = {
   id: number;
   amount: number;
   type?: string;
-  category?: string;
+  category: string;
   date?: string;
 };
 
+export const transactionAdded = createEvent<Omit<Transaction, "id">>();
 export const transactionSubmitted = createEvent();
 export const amountChanged = createEvent<number>();
 export const categoryChanged = createEvent<string>();
 export const dateChanged = createEvent<string>();
+export const categorySelected = createEvent<string>();
 
 export const $transactions = createStore<Transaction[]>([]);
+export const $transactionsFiltered = createStore<Transaction[]>([]);
 export const $amount = createStore(0);
 export const $category = createStore("");
 export const $date = createStore(new Date().toLocaleDateString("ru"));
 
+export const $categoriesSelected = createStore<string[]>([]);
+
 export const $balance = $transactions.map((transactions) => {
-  return transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+  return transactions.reduce((total, transaction) => total + transaction.amount, 0);
 });
 
 const $transaction = combine({
@@ -34,19 +39,51 @@ const $transaction = combine({
   date: $date,
 });
 
+export const $categoriesList = $transactions.map((transactions) => {
+  return transactions.reduce<Record<Transaction["category"], number>>((acc, transaction) => {
+    if (!acc[transaction.category]) {
+      acc[transaction.category] = 0;
+    }
+    acc[transaction.category] += transaction.amount;
+    return acc;
+  }, {});
+});
+
 $amount.on(amountChanged, (_, amount) => amount);
 $date.on(dateChanged, (_, date) => date);
 
 $category.on(categoryChanged, (_, category) => category);
 
+$transactions.on(transactionAdded, (transactions, transaction) => {
+  return [...transactions, { ...transaction, id: transactions.length }];
+});
+
 sample({
   clock: transactionSubmitted,
-  source: { transaction: $transaction, transactions: $transactions },
+  source: $transaction,
+  target: transactionAdded,
+});
 
-  fn: ({ transaction, transactions }) => {
-    return [...transactions, { ...transaction, id: transactions.length }];
+$categoriesSelected.on(categorySelected, (categories, categorySelected) => {
+  if (categories.includes(categorySelected)) {
+    return categories.filter((category) => category !== categorySelected);
+  }
+
+  return [...categories, categorySelected];
+});
+
+sample({
+  clock: [transactionAdded, categorySelected],
+  source: { transactions: $transactions, selectedCategories: $categoriesSelected },
+  fn: ({ transactions, selectedCategories }, _) => {
+    if (selectedCategories.length) {
+      return transactions.filter((transaction) =>
+        selectedCategories.includes(transaction.category),
+      );
+    }
+    return transactions;
   },
-  target: $transactions,
+  target: $transactionsFiltered,
 });
 
 reset({

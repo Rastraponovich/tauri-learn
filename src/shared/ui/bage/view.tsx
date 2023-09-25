c943d11b518ddd210a067e7b8e59796a97c88b2f
Rastraponovich <wilde@bk.ru>
2023-09-25 10:50:05 +0300

    chore: bage improved

--- src/shared/ui/bage/view.tsx
+++ src/shared/ui/bage/view.tsx
@@ -5,8 +5,9 @@ interface BadgeProps {
   children: ReactNode;
   onClick?: () => void;
   onClose?: () => void;
+  selected?: boolean;
   variant?: "success" | "error";
-  size?: "sm" | "md";
+  size?: "sm" | "md" | "xs" | "lg" | "xl";
 }
 
 const variants = {
@@ -15,19 +16,24 @@ const variants = {
 };
 
 const sizes = {
-  sm: "px-2 py-1",
-  md: "px-4 py-2",
+  xs: "px-2 py-1 text-xs",
+  sm: "px-2 py-1 text-sm",
+  md: "px-4 py-2 text-sm",
+  lg: "px-6 py-3 text-base",
+  xl: "px-8 py-4 text-xl",
 };
 
 export const Badge = forwardRef<HTMLDivElement, BadgeProps>(
-  ({ children, variant = "success", size = "md" }, ref) => {
+  ({ children, variant = "success", size = "md", onClick, selected }, ref) => {
     return (
       <div
         ref={ref}
+        onClick={onClick}
         className={clsx(
-          "flex items-center justify-center rounded-3xl text-white outline outline-offset-2 hover:outline-gray-200",
+          "flex cursor-pointer items-center justify-center rounded-3xl text-white outline outline-offset-2 hover:outline-gray-200",
           variants[variant],
           sizes[size],
+          selected && "outline-blue-500",
         )}
       >
         {children}

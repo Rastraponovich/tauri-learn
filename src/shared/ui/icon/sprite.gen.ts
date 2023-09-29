283b7568d0dc6c2f0f6f8ba7226f9044aafcb7c6
Rastraponovich <wilde@bk.ru>
2023-09-29 12:25:53 +0300

    chore: added chevron icon and changed dark bg color

--- src/shared/ui/icon/sprite.gen.ts
+++ src/shared/ui/icon/sprite.gen.ts
@@ -1,8 +1,11 @@
 export interface SpritesMap {
   weather: "sun" | "moon-star";
+  arrows: "chevron-selector-vertical";
 }
 export const SPRITES_META = {
   weather: ["sun", "moon-star"],
+  arrows: ["chevron-selector-vertical"],
 } satisfies {
   weather: Array<"sun" | "moon-star">;
+  arrows: Array<"chevron-selector-vertical">;
 };
